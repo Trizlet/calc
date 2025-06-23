ca08b706fd945ce1d410d3d1daa58d6a860b4c3c
Trizlet <supere0405@gmail.com>
2025-06-23 09:54:53 -0400

    better overflow and unf handling

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,27 @@ fn main() {
         return;
     }
 
-    let expression = args.join(" ");
-    let expression = expression.trim();
-
+    let expression = args.join(" ").trim().to_string();
     // println!("{}",expression);
 
     match meval::eval_str(expression){
-        Ok(result)=>println!("{}",result),
-        Err(e)=>eprintln!("Error: {}",e),
+        Ok(result)=>{
+            if !result.is_finite() || result.abs()>1e12{
+                    eprintln!("Result is undefined or overflowed");
+                    return;
+            }
+
+            let rounded = (result*1e9).round()/1e9;
+
+            if (rounded - rounded.trunc()).abs()<1e-9{
+                println!("{}",rounded.trunc() as i64);
+            }
+            else {
+                println!("{}",rounded);
+            }
+        }
+        Err(e)=>{
+            eprintln!("Error: {}",e);
+        }
     }
 }
